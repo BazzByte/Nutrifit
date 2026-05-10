9f31bafd2fd2a118ebc4c6df59c55c8ee719774f
BazzByte <ahmedbazvip1@gmail.com>
2026-05-10 17:34:20 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -6,12 +6,14 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
     
     GEMINI_API_KEY: str
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+        # مهم: عشان يقبل متغيرات من Railway
+        extra = "ignore"
 
 settings = Settings()
