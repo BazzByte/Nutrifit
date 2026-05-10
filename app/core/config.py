391b309530d6a3ea209c9a6f52342bab95e44f05
BazzByte <ahmedbazvip1@gmail.com>
2026-05-10 14:24:43 +0300

    Create config.py

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "NutriFit API"
+    
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
+    
+    GEMINI_API_KEY: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
