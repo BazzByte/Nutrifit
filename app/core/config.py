1bd23562a1ee03640249f6987540ece34be32ee5
BazzByte <ahmedbazvip1@gmail.com>
2026-05-31 16:51:57 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,13 +2,14 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "NutriFit API"
-    
+
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
     
-    GEMINI_API_KEY: str
+    GROQ_API_KEY: str
 
     class Config:
         env_file = ".env"
